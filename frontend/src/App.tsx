82c7a038e889aa08e107d0195db845e5b41df9d3
Satyam Chaturvedi <satyamchaturvedi71@gmail.com>
2025-07-02 12:28:26 +0530

    add production baseUrl

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,10 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-const baseUrl = "/api"
 function App() {
   const [data, setData] = useState("");
   useEffect(() => {
-    fetch(`${baseUrl}/example`)
+    fetch(`${import.meta.env.VITE_API_BASE_URL}/example`)
       .then((response) => response.json())
       .then((json) => setData(json.message))
       .catch((error) => console.error("Error fetching data:", error));
